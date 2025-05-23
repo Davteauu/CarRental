bcd040aa1c4f65a23292f368ffc3cfcf692d1e25
dawid <dawiddavtyan16@gmail.com>
2025-05-23 14:30:34 +0200

    strona logowania

--- car_rental_angular/src/app/auth/components/login/login.component.ts
+++ car_rental_angular/src/app/auth/components/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    NzSpinModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzLayoutModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  isSpinning: boolean = false;
 }
